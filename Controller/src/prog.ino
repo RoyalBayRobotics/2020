b68c9bd7374d69bde2eb6f98b5ad13833bc06448
Rio6 <rio6.liu@gmail.com>
2020-03-26 21:16:32 -0700

    Add joystick calibration

--- Controller/src/prog.ino
+++ Controller/src/prog.ino
@@ -8,21 +8,33 @@
  * + #define XBOX_WIRELESS_RECEIVER_THIRD_PARTY_PID  0x02A9  // Third party Wireless Gaming Receiver
  */
 
+struct Neutral {
+    int16_t leftX, leftY, rightX, rightY, L2, R2;
+};
+
 USB usb;
 XBOXRECV xbox(&usb);
+Neutral neutrals[4] = {0};
 
-struct {
-    int16_t leftX, leftY, rightX, rightY, L2, R2
-} neutral = {0};
+int16_t clampAdd(int16_t a, int16_t b) {
+    if(a > 0 && b > 32767 - a) {
+        return 32767;
+    } else if(a < 0 && b < -32768 - a) {
+        return -32768;
+    }
+    return a + b;
+}
 
 XboxState xboxState(int id) {
     return XboxState {
-        .leftX = xbox.getAnalogHat(LeftHatX, id),
-        .leftY = xbox.getAnalogHat(LeftHatY, id),
-        .rightX = xbox.getAnalogHat(RightHatX, id),
-        .rightY = xbox.getAnalogHat(RightHatY, id),
-        .L2 = xbox.getButtonPress(L2),
-        .R2 = xbox.getButtonPress(R2),
+
+        .leftX = clampAdd(xbox.getAnalogHat(LeftHatX, id), -neutrals[id].leftX),
+        .leftY = clampAdd(xbox.getAnalogHat(LeftHatY, id), -neutrals[id].leftY),
+        .rightX = clampAdd(xbox.getAnalogHat(RightHatX, id), -neutrals[id].rightX),
+        .rightY = clampAdd(xbox.getAnalogHat(RightHatY, id), -neutrals[id].rightY),
+        .L2 = clampAdd(xbox.getButtonPress(L2, id), -neutrals[id].L2),
+        .R2 = clampAdd(xbox.getButtonPress(R2, id), -neutrals[id].R2),
+
         .buttons =
             MASK(xbox.getButtonPress(X, id), X) | MASK(xbox.getButtonPress(Y, id), Y) |
             MASK(xbox.getButtonPress(A, id), A) | MASK(xbox.getButtonPress(B, id), B) |
@@ -58,6 +70,17 @@ void loop() {
     if(xbox.XboxReceiverConnected) {
         for(int i = 0; i < 4; i++) {
             if(xbox.Xbox360Connected[i]) {
+                if(xbox.getButtonClick(XBOX, i)) {
+                    neutrals[i] = Neutral {
+                        .leftX = xbox.getAnalogHat(LeftHatX, i),
+                        .leftY = xbox.getAnalogHat(LeftHatY, i),
+                        .rightX = xbox.getAnalogHat(RightHatX, i),
+                        .rightY = xbox.getAnalogHat(RightHatY, i),
+                        .L2 = xbox.getButtonPress(L2, i),
+                        .R2 = xbox.getButtonPress(R2, i),
+                    };
+                }
+
                 XboxState state = xboxState(i);
                 uint8_t buff[sizeof(state)];
                 memcpy(buff, &state, sizeof(state));
